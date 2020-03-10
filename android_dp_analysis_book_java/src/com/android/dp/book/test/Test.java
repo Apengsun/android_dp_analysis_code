ac619ce7fe3df31dadadd66020364ff138d4b3f1
Apeng <sunzhipeng1991@126.com>
2020-03-10 20:58:01 +0800

    from PC

--- android_dp_analysis_book_java/src/com/android/dp/book/test/Test.java
+++ android_dp_analysis_book_java/src/com/android/dp/book/test/Test.java
@@ -12,7 +12,7 @@
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,13 +26,90 @@ package com.android.dp.book.test;
 
 public class Test {
     public static void main(String[] args) {
-        ResFinder.put(new ResItem(ResType.COLOR, "color1"), 123);
-        ResFinder.put(new ResItem(ResType.COLOR, "color1"), 123);
-        ResFinder.put(new ResItem(ResType.COLOR, "color2"), 222);
-        ResFinder.put(new ResItem(ResType.COLOR, "color3"), 333);
-        System.out.println("size : " + ResFinder.mResourcesCache.size());
+        //        ResFinder.put(new ResItem(ResType.COLOR, "color1"), 123);
+        //        ResFinder.put(new ResItem(ResType.COLOR, "color1"), 123);
+        //        ResFinder.put(new ResItem(ResType.COLOR, "color2"), 222);
+        //        ResFinder.put(new ResItem(ResType.COLOR, "color3"), 333);
+        //        System.out.println("size : " + ResFinder.mResourcesCache.size());
+        //
+        //        int resId = ResFinder.getResourceId(ResType.COLOR, "color1");
+        //        System.out.println(" color = " + resId);
+        //
+        //        Node node4=new Node();
+        //        node1.value=4;
+        //        Node node3=new Node();
+        //        node3.value=3;
+        //        node3.next=node4;
+        //        Node node2=new Node();
+        //        node2.value=2;
+        //        node2.next=node3;
+        //        Node node1=new Node();
+        //        node1.value=1;
+        //        node1.next=node2;
+        //
+        //       Node node= reserve(node1);
+        //        while (node.next!=null){
+        //            System.out.println(" node = " + node.value);
+        //        }
+        ArrayList test=selfDividing(1, 22);
+        for (int i = 0; i < ; i++) {
 
-        int resId = ResFinder.getResourceId(ResType.COLOR, "color1");
-        System.out.println(" color = " + resId);
+        }
     }
+
+    public static Node reserve(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        Node head;
+        Node cur;
+        boolean isEnd;
+        op(node, isEnd, head, cur);
+        return head
+
+    }
+
+    public static void op(Node node, boolean isEnd, Node head, Node cur) {
+        if (node.next == null) {
+            if (isEnd) {
+                cur.next = node;
+                cur = node;
+            } else {
+                isEnd = true;
+                head = node;
+                cur = node;
+            }
+        } else {
+            op(node.next, isEnd, head, cur);
+        }
+    }
+
+    public static ArrayList<int> selfDividing(int left, int right) {
+        ArrayList<Intger> result = new ArrayList<int>();
+
+        for (int i = left; i <= right; i++) {
+            int b = 0;
+            for (b = i; b > 0; b = b / 10) {
+                if (b % 10 == 0 || i % (n % 10) != 0)
+                    break;
+            }
+            if (b == 0)
+                result.add(i);
+
+        }
+
+        return result;
+
+    }
+
 }
+
+
+class Node {
+    public int value;
+    public Node next;
+
+}
+
+
